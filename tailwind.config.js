3fcfdd3b7f53e28535ae5f425b0f4411640533da
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-04-29 21:09:33 +0700

    modified:   package-lock.json 	modified:   package.json 	modified:   resources/css/app.css 	modified:   resources/js/app.js 	modified:   resources/views/admin/layouts/main.blade.php 	modified:   tailwind.config.js 	modified:   vite.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
     ],
     safelist: [
         "w-64",
@@ -188,5 +189,5 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [require("flowbite/plugin")],
 };
