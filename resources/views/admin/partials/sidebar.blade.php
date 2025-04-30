af9cadab2291b3d5cc4b68b0ecda2dacd1bcdea9
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-04-30 09:40:22 +0700

    modified:   resources/views/admin/partials/sidebar.blade.php

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -83,41 +83,7 @@
                                 class="bg-fuchsia-50 text-fuchsia-800 ml-auto text-sm font-medium inline-flex items-center justify-center px-2 rounded-full">Pro</span>
                         </a>
                     </li>
-                    <li>
-                        <a href="https://demos.creative-tim.com/soft-ui-flowbite-pro/kanban/"
-                            class="flex items-center py-2.5 px-4 text-base font-normal text-dark-500 rounded-lg hover:bg-gray-200 group transition-all duration-200"
-                            sidebar-toggle-collapse>
-                            <div class="bg-white shadow-lg shadow-gray-300 {{ if eq $page_slug " inbox" }}
-                                bg-fuchsia-500 !text-white {{ end }} text-dark-700 w-8 h-8 p-2.5 mr-1 rounded-lg
-                                text-center grid place-items-center">
-                                <svg width="12px" height="12px" viewBox="0 0 46 42" version="1.1"
-                                    xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-                                    <title>customer-support</title>
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <g transform="translate(-1717.000000, -291.000000)" fill="currentColor"
-                                            fill-rule="nonzero">
-                                            <g transform="translate(1716.000000, 291.000000)">
-                                                <g transform="translate(1.000000, 0.000000)">
-                                                    <path class="color-background"
-                                                        d="M45,0 L26,0 C25.447,0 25,0.447 25,1 L25,20 C25,20.379 25.214,20.725 25.553,20.895 C25.694,20.965 25.848,21 26,21 C26.212,21 26.424,20.933 26.6,20.8 L34.333,15 L45,15 C45.553,15 46,14.553 46,14 L46,1 C46,0.447 45.553,0 45,0 Z"
-                                                        opacity="0.59858631"></path>
-                                                    <path class="color-foreground"
-                                                        d="M22.883,32.86 C20.761,32.012 17.324,31 13,31 C8.676,31 5.239,32.012 3.116,32.86 C1.224,33.619 0,35.438 0,37.494 L0,41 C0,41.553 0.447,42 1,42 L25,42 C25.553,42 26,41.553 26,41 L26,37.494 C26,35.438 24.776,33.619 22.883,32.86 Z">
-                                                    </path>
-                                                    <path class="color-foreground"
-                                                        d="M13,28 C17.432,28 21,22.529 21,18 C21,13.589 17.411,10 13,10 C8.589,10 5,13.589 5,18 C5,22.529 8.568,28 13,28 Z">
-                                                    </path>
-                                                </g>
-                                            </g>
-                                        </g>
-                                    </g>
-                                </svg>
-                            </div>
-                            <span class="ml-3 text-dark-500 text-sm font-light" sidebar-toggle-item>Inbox</span>
-                            <span
-                                class="bg-fuchsia-50 text-fuchsia-800 ml-auto text-sm font-medium inline-flex items-center justify-center px-2 rounded-full">Pro</span>
-                        </a>
-                    </li>
+
                 </ul>
                 <hr class="border-0 h-px bg-gradient-to-r from-gray-100 via-gray-300 to-gray-100">
                 <div class="pt-2">
