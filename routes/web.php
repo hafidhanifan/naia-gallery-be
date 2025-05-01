88ba61735e94751003e95cbc1b1685afd71d12fa
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-01 19:55:55 +0700

    new file:   resources/views/admin/layouts/auth.blade.php 	new file:   resources/views/admin/sign-in.blade.php 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/dashborad', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/products', [ProductsController::class, 'index'])->name('products');
 // Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
 // });
