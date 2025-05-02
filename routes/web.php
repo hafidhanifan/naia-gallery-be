02b1b01d479ceb0f32123cc78283ef51626e9446
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-02 21:53:06 +0700

    Installing Fortify for authentication

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductsController;
 use Illuminate\Support\Facades\Route;
 
-
 Route::get('/dashborad', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/products', [ProductsController::class, 'index'])->name('products');
 // Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
