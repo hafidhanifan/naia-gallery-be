0784ed01b32c7b7d8bbb0a8e8b21af02bec4b60b
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-03 00:12:35 +0700

    modified:   app/Http/Controllers/Admin/LoginController.php

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -10,4 +10,20 @@ class LoginController extends Controller
     public function showLoginPage () {
         return view('admin.sign-in');
     }
+
+    public function handleLogin () {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate(); // Menghindari session fixation
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'Email atau password salah',
+        ])->withInput();
+    }
 }
