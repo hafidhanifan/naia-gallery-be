03416db75bd67583900482559a63625bd0310380
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-02 22:33:50 +0700

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\ProductsController;
-use Illuminate\Support\Facades\Route;
 
-Route::get('/dashborad', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/products', [ProductsController::class, 'index'])->name('products');
+Route::middleware('guest')->group(function() {
+    Route::get('/',[LoginController::class, 'showLoginPage'])->name('login');
+    Route::post('/', [LoginController::class, 'login']);
+});
+
+Route::middleware('auth')->group(function(){
+    Route::get('/dashborad', [DashboardController::class, 'showDashboardPage'])->name('dashboard');
+    Route::get('/products', [ProductsController::class, 'showProductsPage'])->name('products');
+});
 // Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
 // });
