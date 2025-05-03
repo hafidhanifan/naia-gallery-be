6590a3699734e55a197421867b55235f4dcbd51f
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-03 14:13:38 +0700

    modified:   app/Http/Controllers/Admin/ProductsController.php

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -1,10 +1,34 @@
 <?php 
 
 namespace App\Http\Controllers\Admin;
+use App\Models\Dress;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+
 
 class ProductsController extends Controller {
     public function showProductsPage() {
-        return view('admin.products');
+        $dresses = Dress::all();
+        return view('admin.products', compact('dresses'));
+    }
+
+    public function addProduct(Request $request) {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price_per_day' => 'required|numeric|min:0',
+            'stock' => 'required|integer|min:0',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg|max:2048',
+        ]);
+
+        if ($request->hasFile('image')){
+            $valiated['image'] = $request->file('image')->store('dresses', 'public');
+        }
+
+        Dress::create($validated);
+
+
+
+        return redirect()->route('admin.products')->with('success', 'Dress berhasil ditambahkan');
     }
 }
