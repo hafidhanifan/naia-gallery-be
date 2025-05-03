d5b360ddc9575fb9817a53252aa667268aadd33b
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-03 15:31:00 +0700

    modified:   app/Http/Controllers/Admin/ProductsController.php

--- app/Http/Controllers/Admin/ProductsController.php
+++ app/Http/Controllers/Admin/ProductsController.php
@@ -28,6 +28,6 @@ class ProductsController extends Controller {
 
         Dress::create($validated);
 
-        return redirect()->route('admin.products')->with('success', 'Dress berhasil ditambahkan');
+        return redirect()->route('productsPage')->with('success', 'Dress berhasil ditambahkan');
     }
 }
