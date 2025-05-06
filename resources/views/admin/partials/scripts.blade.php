e36602672a05c066c9fbfe86ea9b395279fcf9e6
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-06 08:17:29 +0700

    modified:   resources/views/admin/partials/scripts.blade.php

--- resources/views/admin/partials/scripts.blade.php
+++ resources/views/admin/partials/scripts.blade.php
@@ -1,2 +1,3 @@
 <script async="" defer="" src="https://buttons.github.io/buttons.js"></script>
 <script src="{{ asset('js/app.bundle.js') }}"></script>
+<script src="//unpkg.com/alpinejs" defer></script>
