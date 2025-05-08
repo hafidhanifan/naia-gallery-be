45c17404dd487fcc58dbb55544d93bc7ea39d4e2
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-08 13:35:03 +0700

    modified:   resources/views/admin/categories.blade.php

--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -129,8 +129,8 @@
                                 <div class="text-base font-semibold text-gray-900">{{ $category->name }}</div>
                             </td>
                             <td class="p-4 space-x-2 whitespace-nowrap lg:p-5">
-                                <button type="button" data-modal-target="product-modal"
-                                    data-modal-toggle="product-modal"
+                                <button type="button" data-modal-target="edit-modal-{{ $category->id }}"
+                                    data-modal-toggle="edit-modal-{{ $category->id }}"
                                     class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300 hover:text-gray-900 hover:scale-[1.02] transition-all">
                                     <svg class="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg">
@@ -156,6 +156,56 @@
                                 </button>
                             </td>
                         </tr>
+
+                        <!-- Edit Product Modal -->
+                        <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
+                            id="edit-modal-{{ $category->id }}">
+                            <div class="relative px-4 w-full max-w-2xl h-full md:h-auto">
+                                <!-- Modal content -->
+                                <div class="relative bg-white rounded-2xl shadow-xl shadow-gray-700">
+                                    <!-- Modal header -->
+                                    <div class="flex justify-between items-start p-5 rounded-t border-b">
+                                        <h3 class="text-xl font-semibold">
+                                            Edit product
+                                        </h3>
+                                        <button type="button"
+                                            class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
+                                            data-modal-toggle="edit-modal-{{ $category->id }}">
+                                            <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                                                xmlns="http://www.w3.org/2000/svg">
+                                                <path fill-rule="evenodd"
+                                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                    <!-- Modal body -->
+                                    <div class="p-6 space-y-6">
+                                        <form action="{{ route('editCategory', $category->id) }}" method="POST">
+                                            @csrf
+                                            <div class="grid grid-cols-1">
+                                                <div class="col-span-6 sm:col-span-3">
+                                                    <label for="product-name"
+                                                        class="block mb-2 text-sm font-medium text-gray-900">Category
+                                                        Name</label>
+                                                    <input type="text" name="product-name" id="product-name"
+                                                        class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                                                        placeholder="Wedding Dress”" value="{{ $category->name }}"
+                                                        required>
+                                                </div>
+                                            </div>
+                                            <!-- Modal footer -->
+                                            <div class="p-6 rounded-b border-t border-gray-200">
+                                                <button
+                                                    class="text-white font-medium text-sm px-5 py-2.5 text-center rounded-lg bg-gradient-to-br from-pink-500 to-voilet-500 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
+                                                    type="submit">Save</button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
                         @endforeach
                     </tbody>
                 </table>
