2eafda6f1eebf9e54de7327c627b064c4d93ad93
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-05-08 12:36:33 +0700

    modified:   resources/js/app.js 	modified:   resources/views/admin/categories.blade.php

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,32 @@ import "./bootstrap";
 import "flowbite";
 import "./sidebar";
 // import "./charts";
+
+// let categoryModal, dressModal;
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     categoryModal = new Modal(document.getElementById("editCategoryModal"));
+//     // dressModal = new Modal(document.getElementById('editDressModal'));
+// });
+
+// function openCategoryEditModal(id, name) {
+//     document.getElementById("editCategoryName").value = name;
+//     document.getElementById(
+//         "editCategoryForm"
+//     ).action = `/categories/edit/${id}`;
+//     categoryModal.show();
+// }
+// let categoryModal;
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Inisialisasi modal Flowbite
+//     categoryModal = new Modal(document.getElementById("editCategoryModal"));
+// });
+
+// function openCategoryEditModal(id, name) {
+//     document.getElementById("editCategoryName").value = name;
+//     document.getElementById(
+//         "editCategoryForm"
+//     ).action = `/categories/edit/${id}`;
+//     categoryModal.show(); // Menampilkan modal
+// }
--- resources/views/admin/categories.blade.php
+++ resources/views/admin/categories.blade.php
@@ -187,187 +187,119 @@
             </div>
             <!-- Modal body -->
             <div class="p-6 space-y-6">
-                <form action="#">
-                    <div class="grid grid-cols-6 gap-6">
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="product-name" class="block mb-2 text-sm font-medium text-gray-900">Product
-                                Name</label>
-                            <input type="text" name="product-name" id="product-name"
-                                class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                placeholder="Apple Imac 27”" required>
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="category" class="block mb-2 text-sm font-medium text-gray-900">Category</label>
-                            <input type="text" name="category" id="category"
-                                class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                placeholder="Electronics" required>
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="brand" class="block mb-2 text-sm font-medium text-gray-900">Brand</label>
-                            <input type="text" name="brand" id="brand"
-                                class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                placeholder="Apple" required>
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Price</label>
-                            <input type="number" name="price" id="price"
-                                class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                placeholder="$2300" required>
-                        </div>
-                        <div class="col-span-full">
-                            <label for="product-details" class="block mb-2 text-sm font-medium text-gray-900">Product
-                                Details</label>
-                            <textarea id="product-details" rows="6"
-                                class="block p-4 w-full text-gray-900 border border-gray-300 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300"
-                                placeholder="e.g. 3.8GHz 8-core 10th-generation Intel Core i7 processor, Turbo Boost up to 5.0GHz, Ram 16 GB DDR4 2300Mhz"></textarea>
-                        </div>
-                    </div>
-                    <div class="flex my-4 space-x-5">
-                        <div>
-                            <img src="/images/products/apple-imac-1.png" class="h-24" alt="imac image">
-                            <a href="#" class="cursor-pointer"><svg class="-mt-5 w-6 h-6 text-red-600"
-                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                        clip-rule="evenodd"></path>
-                                </svg></a>
-                        </div>
-                        <div>
-                            <img src="/images/products/apple-imac-2.png" class="h-24" alt="imac image">
-                            <a href="#" class="cursor-pointer"><svg class="-mt-5 w-6 h-6 text-red-600"
-                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                        clip-rule="evenodd"></path>
-                                </svg></a>
-                        </div>
-                        <div>
-                            <img src="/images/products/apple-imac-3.png" class="h-24" alt="imac image">
-                            <a href="#" class="cursor-pointer"><svg class="-mt-5 w-6 h-6 text-red-600"
-                                    fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                                        clip-rule="evenodd"></path>
-                                </svg></a>
-                        </div>
-                    </div>
-                    <div class="flex justify-center items-center w-full">
-                        <label
-                            class="flex flex-col w-full h-32 rounded border-2 border-gray-300 border-dashed cursor-pointer hover:bg-gray-50">
-                            <div class="flex flex-col justify-center items-center pt-5 pb-6">
-                                <svg class="w-10 h-10 text-gray-400" fill="none" stroke="currentColor"
-                                    viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"></path>
-                                </svg>
-                                <p class="py-1 text-sm text-gray-600">Upload a file or drag and drop</p>
-                                <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                            </div>
-                            <input type="file" class="hidden" />
-                        </label>
-                    </div>
-            </div>
-            <!-- Modal footer -->
-            <div class="p-6 rounded-b border-t border-gray-200">
-                <button
-                    class="text-white font-medium text-sm px-5 py-2.5 text-center rounded-lg bg-gradient-to-br from-pink-500 to-voilet-500 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
-                    type="submit">Save all</button>
-            </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<!-- Add Product Modal -->
-<div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
-    id="add-product-modal">
-    <div class="relative px-4 w-full max-w-2xl h-full md:h-auto">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-2xl shadow-lg">
-            <!-- Modal header -->
-            <div class="flex justify-between items-start p-5 rounded-t border-b">
-                <h3 class="text-xl font-semibold">
-                    Add category
-                </h3>
-                <button type="button"
-                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
-                    data-modal-toggle="add-product-modal">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
-                    </svg>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <div class="p-6 space-y-6">
-                <form action="{{ route('addCategory') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ route('editCategory', $category->id) }}" method="POST">
                     @csrf
                     <div class="grid grid-cols-1">
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Category
+                            <label for="product-name" class="block mb-2 text-sm font-medium text-gray-900">Category
                                 Name</label>
-                            <input type="text" name="name" id="product-name"
+                            <input type="text" name="product-name" id="product-name"
                                 class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                placeholder="Wedding Dress" required>
+                                placeholder="Wedding Dress”" value="{{ $category->name }}" required>
                         </div>
                     </div>
+                    <!-- Modal footer -->
+                    <div class="p-6 rounded-b border-t border-gray-200">
+                        <button
+                            class="text-white font-medium text-sm px-5 py-2.5 text-center rounded-lg bg-gradient-to-br from-pink-500 to-voilet-500 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
+                            type="submit">Save</button>
+                    </div>
+                </form>
             </div>
-            <!-- Modal footer -->
-            <div class="p-6 rounded-b border-t border-gray-200">
-                <button
-                    class="text-white font-medium text-sm px-5 py-2.5 text-center rounded-lg bg-gradient-to-br from-pink-500 to-voilet-500 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
-                    type="submit">Add category</button>
-            </div>
-            </form>
         </div>
     </div>
-</div>
 
-<!-- Delete Product Modal -->
-<div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
-    id="delete-product-modal">
-    <div class="relative px-4 w-full max-w-md h-full md:h-auto">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-2xl shadow-lg">
-            <!-- Modal header -->
-            <div class="flex justify-end p-2">
-                <button type="button"
-                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
-                    data-modal-toggle="delete-product-modal">
-                    <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd"></path>
+    <!-- Add Product Modal -->
+    <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
+        id="add-product-modal">
+        <div class="relative px-4 w-full max-w-2xl h-full md:h-auto">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-2xl shadow-lg">
+                <!-- Modal header -->
+                <div class="flex justify-between items-start p-5 rounded-t border-b">
+                    <h3 class="text-xl font-semibold">
+                        Add category
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
+                        data-modal-toggle="add-product-modal">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 space-y-6">
+                    <form action="{{ route('addCategory') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="grid grid-cols-1">
+                            <div class="col-span-6 sm:col-span-3">
+                                <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Category
+                                    Name</label>
+                                <input type="text" name="name" id="product-name"
+                                    class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
+                                    placeholder="Wedding Dress" required>
+                            </div>
+                        </div>
+                </div>
+                <!-- Modal footer -->
+                <div class="p-6 rounded-b border-t border-gray-200">
+                    <button
+                        class="text-white font-medium text-sm px-5 py-2.5 text-center rounded-lg bg-gradient-to-br from-pink-500 to-voilet-500 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform"
+                        type="submit">Add category</button>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- Delete Product Modal -->
+    <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
+        id="delete-product-modal">
+        <div class="relative px-4 w-full max-w-md h-full md:h-auto">
+            <!-- Modal content -->
+            <div class="relative bg-white rounded-2xl shadow-lg">
+                <!-- Modal header -->
+                <div class="flex justify-end p-2">
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
+                        data-modal-toggle="delete-product-modal">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="p-6 pt-0 text-center">
+                    <svg class="mx-auto w-20 h-20 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                </button>
-            </div>
-            <!-- Modal body -->
-            <div class="p-6 pt-0 text-center">
-                <svg class="mx-auto w-20 h-20 text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                </svg>
-                <h3 class="mt-5 mb-6 text-xl font-normal text-gray-500">Are you sure you want to delete this product?
-                </h3>
-                <a href="#"
-                    class="text-white bg-gradient-to-br from-red-400 to-red-600 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform">
-                    Yes, I'm sure
-                </a>
-                <a href="#"
-                    class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center hover:scale-[1.02] transition-transform"
-                    data-modal-toggle="delete-product-modal">
-                    No, cancel
-                </a>
+                    <h3 class="mt-5 mb-6 text-xl font-normal text-gray-500">Are you sure you want to delete this
+                        product?
+                    </h3>
+                    <a href="#"
+                        class="text-white bg-gradient-to-br from-red-400 to-red-600 font-medium rounded-lg text-base inline-flex items-center px-3 py-2.5 text-center mr-2 shadow-md shadow-gray-300 hover:scale-[1.02] transition-transform">
+                        Yes, I'm sure
+                    </a>
+                    <a href="#"
+                        class="text-gray-900 bg-white hover:bg-gray-100 border border-gray-200 font-medium inline-flex items-center rounded-lg text-base px-3 py-2.5 text-center hover:scale-[1.02] transition-transform"
+                        data-modal-toggle="delete-product-modal">
+                        No, cancel
+                    </a>
+                </div>
             </div>
         </div>
     </div>
-</div>
 
-@foreach (['success', 'error', 'info', 'warning'] as $msg)
-@if(session($msg))
-<x-alert :type="$msg" :message="session($msg)" />
-@endif
-@endforeach
-@endsection
+    @foreach (['success', 'error', 'info', 'warning'] as $msg)
+    @if(session($msg))
+    <x-alert :type="$msg" :message="session($msg)" />
+    @endif
+    @endforeach
+    @endsection
