fe5bfb16731e8500e2e1363169ff351e3f821614
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-06-11 20:20:06 +0700

    modified:   resources/views/admin/products.blade.php

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -156,7 +156,7 @@
                             </td>
                             <td class="p-4 space-x-2 whitespace-nowrap lg:p-5">
                                 <button type="button" data-modal-target="edit-dress-{{ $dress->id }}"
-                                    data-modal-toggle="product-modal"
+                                    data-modal-toggle="edit-dress-{{ $dress->id }}"
                                     class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-300 hover:text-gray-900 hover:scale-[1.02] transition-all">
                                     <svg class="mr-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg">
@@ -192,7 +192,7 @@
 
 <!-- Edit Product Modal -->
 <div class="hidden overflow-y-auto overflow-x-hidden fixed right-0 left-0 top-4 z-50 justify-center items-center md:inset-0 h-modal sm:h-full"
-    id="product-modal">
+    id="edit-dress-{{ $dress->id }}">
     <div class="relative px-4 w-full max-w-2xl h-full md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-2xl shadow-xl shadow-gray-700">
@@ -203,7 +203,7 @@
                 </h3>
                 <button type="button"
                     class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-2xl text-sm p-1.5 ml-auto inline-flex items-center"
-                    data-modal-toggle="product-modal">
+                    data-modal-toggle="edit-dress-{{ $dress->id }}">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
                             d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
