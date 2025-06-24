7aead67770ba80a106eafc379c810c905603bc17
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-06-24 20:40:38 +0700

    modified:   resources/views/admin/products.blade.php

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -220,10 +220,16 @@
                                                 </div>
                                                 <div class="col-span-6 sm:col-span-3">
                                                     <label for="category_id"
-                                                        class="block mb-2 text-sm font-medium text-gray-900">Category</label>
-                                                    <input type="text" name="category" id="category"
-                                                        class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                                        placeholder="Electronics" required>
+                                                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select
+                                                        an
+                                                        option</label>
+                                                    <select id="category_id" name="category_id"
+                                                        class="bg-transparent border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5">
+                                                        @foreach($categories as $category)
+                                                        <option value="{{ $category->id }}">{{ $category->name }}
+                                                        </option>
+                                                        @endforeach
+                                                    </select>
                                                 </div>
                                                 <div class="col-span-6 sm:col-span-3">
                                                     <label for="brand"
