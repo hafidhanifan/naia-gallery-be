078b5cd01be5e073bc048a284d32d43e40fc4fe7
Hafid Hanifan <hafid.hanifan123@gmail.com>
2025-06-24 20:24:09 +0700

    modified:   resources/views/admin/products.blade.php

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -211,15 +211,15 @@
                                             @method('POST')
                                             <div class="grid grid-cols-6 gap-6">
                                                 <div class="col-span-6 sm:col-span-3">
-                                                    <label for="product-name"
-                                                        class="block mb-2 text-sm font-medium text-gray-900">Product
+                                                    <label for="name"
+                                                        class="block mb-2 text-sm font-medium text-gray-900">Dress
                                                         Name</label>
-                                                    <input type="text" name="product-name" id="product-name"
+                                                    <input type="text" name="name" id="name"
                                                         class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
-                                                        placeholder="Apple Imac 27”" required>
+                                                        value="{{ old('name', $dress->name) }}" required>
                                                 </div>
                                                 <div class="col-span-6 sm:col-span-3">
-                                                    <label for="category"
+                                                    <label for="category_id"
                                                         class="block mb-2 text-sm font-medium text-gray-900">Category</label>
                                                     <input type="text" name="category" id="category"
                                                         class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5"
